726d7d4ca98cce81b32c19dcf469521298dd647f
Twostap <tyler.ostapyk@umanitoba.ca>
2026-02-17 13:15:10 -0600

    Update srcalculator.py

--- srcalculator.py
+++ srcalculator.py
@@ -412,9 +412,8 @@ def recordsdata():
        df.head()
 
        Medlinedf = df.query('Medline - @TotalMedline <= 99 & Medline - @TotalMedline > -99 & Topics.str.contains(@Topics)')
-       TopicsUniquedf = df.query('Topics.str.contains(@Topics)')
-       Uniquedf = TopicsUniquedf[UniquePercent].mean(numeric_only=True)
-       print(Uniquedf)
+       TopicsUnique = df.query('Topics.str.contains(@Topics)').[UniquePercent].mean(numeric_only=True)
+       print(TopicsUnique)
         
        if DatabaseSelection1=="selectone":
            display1 = "display: none;"
@@ -841,6 +840,7 @@ def recordsdata():
 
 if __name__=='__main__':
    app.run()
+
 
 
 
