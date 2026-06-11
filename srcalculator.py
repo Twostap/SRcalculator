0a4ad6a28095b7b1e056b6db96057ecfe8ba2bd5
Twostap <tyler.ostapyk@umanitoba.ca>
2026-06-11 13:33:01 -0500

    Update srcalculator.py

--- srcalculator.py
+++ srcalculator.py
@@ -759,10 +759,7 @@ def recordsdata():
        elif 'Topic_0' in globals(): 
            Topicdf = df.query('Topics.str.contains(@Topic_0)')      
 
-       if not Topicdf:
-           Medlinedf = "999999"
-       else:      
-           Medlinedf = Topicdf.query('MedlineAverage - @TotalMedline <= 99 & MedlineAverage - @TotalMedline > -99')
+       Medlinedf = Topicdf.query('MedlineAverage - @TotalMedline <= 99 & MedlineAverage - @TotalMedline > -99')
         
        if CINAHLDatabase=="on":
            CINAHLChecked = "checked"
